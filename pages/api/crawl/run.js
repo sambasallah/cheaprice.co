47f181f41c4cba48d52b9b08a8d6afee80e64615
sambasallah <sambasallah10@gmail.com>
2020-08-25 02:19:10 +0000

    Added changes

--- pages/api/crawl/run.js
+++ pages/api/crawl/run.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 
 export default async (req, res) => {
     if(req.method === 'POST') {
-        await firebase.collection('products').limit(1)
+        await firebase.collection('products').limit(2)
         .get()
         .then(async (snaps) => {
           let products = [];
@@ -12,30 +12,25 @@ export default async (req, res) => {
            })
            // https://mp001iwsca.execute-api.eu-west-1.amazonaws.com/dev/crawl
           
-           await axios.post('https://mp001iwsca.execute-api.eu-west-1.amazonaws.com/dev/crawl',JSON.stringify({products}),{headers: {'Content-Type': 'application/json'}})
-           .then((resp) => {
-             res.json(resp);
-           })
+            axios.post('https://mp001iwsca.execute-api.eu-west-1.amazonaws.com/dev/crawl',JSON.stringify({products}),{headers: {'Content-Type': 'application/json'}})
            .catch((err) => {
                // send email
-              //  axios.post( process.env.NODE_ENV === 'development'? 
-              //  `${process.env.NEXT_PUBLIC_LOCAL_SERVER}/api/sendmail/crawlerror` : 
-              //  `${process.env.NEXT_PUBLIC_LIVE_SERVER}/api/sendmail/crawlerror`,{headers: {'Content-Type': 'application/json'}})
-              //  .catch((err) => {
-              //     console.log('Error sending crawl error message');
-              //  });
-              console.log(err)
-              res.json({err: err})
+               axios.post( process.env.NODE_ENV === 'development'? 
+               `${process.env.NEXT_PUBLIC_LOCAL_SERVER}/api/sendmail/crawlerror` : 
+               `${process.env.NEXT_PUBLIC_LIVE_SERVER}/api/sendmail/crawlerror`,{headers: {'Content-Type': 'application/json'}})
+               .catch((err) => {
+                  console.log('Error sending crawl error message');
+               });
            });
-          //  res.json({info: 'Crawl Started'});
+           res.json({info: 'Crawl Started'});
         }).catch((err) => {
           // send email
-          // axios.post( process.env.NODE_ENV === 'development'? 
-          // `${process.env.NEXT_PUBLIC_LOCAL_SERVER}/api/sendmail/producterror` : 
-          // `${process.env.NEXT_PUBLIC_LIVE_SERVER}/api/sendmail/producterror`,{headers: {'Content-Type': 'application/json'}})
-          // .catch((err) => {
-          //    console.log('Error sending crawl error message');
-          // });
+          axios.post( process.env.NODE_ENV === 'development'? 
+          `${process.env.NEXT_PUBLIC_LOCAL_SERVER}/api/sendmail/producterror` : 
+          `${process.env.NEXT_PUBLIC_LIVE_SERVER}/api/sendmail/producterror`,{headers: {'Content-Type': 'application/json'}})
+          .catch((err) => {
+             console.log('Error sending crawl error message');
+          });
         res.json({message: 'Error getting data'});
         });
     
