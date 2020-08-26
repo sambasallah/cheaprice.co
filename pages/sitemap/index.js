5466d0db14e3d4b779cb5300f0f7e8c2911134ab
sambasallah <sambasallah10@gmail.com>
2020-08-26 18:43:30 +0000

    added

--- pages/sitemap/index.js
+++ pages/sitemap/index.js
@@ -1,49 +1,9 @@
 import React from 'react';
+import SiteMapXML from './sitemap';
 export default class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
     res.setHeader("Content-Type", "text/xml");
-    res.write(sitemapXml(`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" />
-
-<url>
-  <loc>https://cheaprice.co/</loc>
-  <lastmod>2020-08-26T18:08:50+00:00</lastmod>
-  <priority>1.00</priority>
-</url>
-<url>
-  <loc>https://cheaprice.co/deals</loc>
-  <lastmod>2020-08-26T18:08:50+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://cheaprice.co/privacy</loc>
-  <lastmod>2020-08-26T18:08:50+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://cheaprice.co/faqs</loc>
-  <lastmod>2020-08-26T18:08:50+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://cheaprice.co/contact</loc>
-  <lastmod>2020-08-26T18:08:50+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://cheaprice.co/tracking</loc>
-  <lastmod>2020-08-26T18:08:50+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-
-
-</urlset>
-    `));
+    res.write(SiteMapXML);
     res.end();
   }
 }
