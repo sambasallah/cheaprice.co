1c9019e70fb7ebafcaa53e15b1f8cf53375ec734
sambasallah <sambasallah10@gmail.com>
2020-09-05 23:57:12 +0000

    added changes

--- pages/api/products/delete.js
+++ pages/api/products/delete.js
@@ -1,8 +1,8 @@
 
 import firebase from '../firebase/firebase';
 export default async (req, res) => {
-    await firebase.collection('prices')
-    .where('id','==','fjJAJKDSJFKJKJjskjfkjakjskfkJKJAKJFFUiuR2skjfk')
+    await firebase.collection('products')
+    .where('id','==','20HGHG82748SJ2893RSDJDKFhHSJH23hfweirworuUIWF33u882374')
     .get()
     .then((snap) => {
         snap.forEach((doc) => {
--- pages/components/header/Header.js
+++ pages/components/header/Header.js
@@ -37,6 +37,8 @@ const Header = ({title,description,url,image, type}) => {
             <meta property="og:image" content={image} />
             <title>{title}</title>
             <script data-ad-client="ca-pub-7391905567078145" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script>var ezoicId = 212199;</script>
+            <script type="text/javascript" src="//go.ezoic.net/ezoic/ezoic.js"></script>
             <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-174290712-1"
