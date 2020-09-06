37460f5c1f8eed620fd5c6acd7a0b10c19c7b512
sambasallah <sambasallah10@gmail.com>
2020-09-06 01:44:53 +0000

    added changes

--- pages/api/dailydrops/index.js
+++ pages/api/dailydrops/index.js
@@ -24,7 +24,7 @@ export default async (req, res) => {
         .get()
         .then((snap) => {
             snap.forEach((doc) => {
-                if(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)) {
+                if(new Date(Date.now() - 4 * 24 * 60 * 60 * 1000)) {
                    drops.push(doc.data().id);
                }
             });
