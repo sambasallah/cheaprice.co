13d0e93febd532f35284ed99ff3963297b34981d
sambasallah <sambasallah10@gmail.com>
2020-09-12 16:08:24 +0000

    added changes

--- pages/deals/index.js
+++ pages/deals/index.js
@@ -94,17 +94,6 @@ const Deals = ({data}) => {
                               )
                           })}
                         </div>
-                        <ins
-                            className="adsbygoogle adbanner-customize"
-                            style={{
-                                display: "block",
-                                margin: "20px auto"
-                            }}
-                            data-ad-client="ca-pub-7391905567078145"
-                            data-ad-slot="1723273533"
-                            data-ad-format="auto"
-                            data-full-width-responsive="true"
-                            />
                     </div>
                 </div>
             </main>
