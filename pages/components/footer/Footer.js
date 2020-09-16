524e5b981ea07ac1ab820ec99bbcde1d78e6d9ab
sambasallah <sambasallah10@gmail.com>
2020-09-16 00:10:39 +0000

    added changes

--- pages/components/footer/Footer.js
+++ pages/components/footer/Footer.js
@@ -6,7 +6,6 @@ const Footer = () => {
         <>
          <footer className="footer__section">
                 <div>
-                
                 <h3>CHEAPRICE <FiTrendingDown /></h3>
                 <p>CHEAPRICE allows you to check historic prices of products from US based 
                     E-commerce stores for free. This is easy to use price tracker for US based 
@@ -15,15 +14,6 @@ const Footer = () => {
                     constantly waiting for a day or so will help you save 100s of dollars. Cheaprice.co 
                     will help you save money!</p>
                 </div>
-                <ins
-                    className="adsbygoogle adbanner-customize"
-                    style={{
-                        display: "block"
-                    }}
-                    data-ad-client="ca-pub-7391905567078145"
-                    data-ad-slot="1723273533"
-                    data-full-width-responsive="true"
-                />
                 <div className="copyright">
                 <span className="copyright__text">&copy; 2020 CHEAPRICE <FiTrendingDown />. All Rights Reserved</span>
                  <ul className="social__icons">
